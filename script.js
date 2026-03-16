696f9caee833a1d6c111bb8801f141a7e70e693e
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-03-16 09:00:22 -0300

    fix - consertei o bug de sempre somar sem simbolo com uma variavel que pede um simbolo para a conta

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let calculo = " ";
 let display = document.querySelector("h4");
+let verifica = true;
 
 function atualizarDisplay(){
     display.textContent = calculo;
 }
 
 function adicionarnumero(numero){
-    calculo += numero;
-    atualizarDisplay();
+    if (verifica == true){
+     calculo += numero;
+    atualizarDisplay();   
+    }
 }
 
 function adicionarsimbolo(simbolo){
     calculo += simbolo;
     atualizarDisplay();
+    verifica = true;
 }
 
 function calcular(){
-    let conta = calculo.replace('x', '*');
-    calculo = eval(conta);
-    atualizarDisplay();
+    let conta = calculo.replace('x','*');
+    if (verifica == true){
+        calculo = eval(conta);
+        atualizarDisplay();  
+    }
+    verifica = false;
+    
 }
